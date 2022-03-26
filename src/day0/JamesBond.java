d09aebc967c96f26cda95155291f8f72762ccb6d
MrGant <98862439+MrGant@users.noreply.github.com>
2022-03-26 16:31:41 -0400

    Update JamesBond.java

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -1,7 +1,7 @@
 package day0;
 public class JamesBond{
  
-  public class void main(String [] args){
+  public static void main(String [] args){
     
     System.out.println("James Bond hacked the project");
     
